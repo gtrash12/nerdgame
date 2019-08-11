b79b48209255a02aa34a974a2a7d6a849bff81ab
jinhee-PC\jinhee <gtrash12@naver.com>
2019-08-11 21:02:00 +0900

    하단 UI 스크롤 기능 추가

---
+++ Assets/Script/UIScrollScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIScrollScript : MonoBehaviour
+{
+    public Vector3 pivot;
+    private RectTransform scrollBox;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scrollBox = GetComponent<RectTransform>();
+        pivot = Vector3.zero;
+        pivot.y = 200;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        scrollBox.position = Vector3.Lerp(scrollBox.position, pivot, Time.deltaTime * 4);
+    }
+
+    public void changePivot(RectTransform t)
+    {
+        pivot = t.localPosition;
+        pivot.x *= -1;
+        pivot.y = 200;
+    }
+}
