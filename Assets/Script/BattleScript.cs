dae2ab2956d2b68a4606274306df74217796cbeb
jinhee-PC\jinhee <gtrash12@naver.com>
2019-08-13 22:10:51 +0900

    피격애니메이션 추가

--- Assets/Script/BattleScript.cs
+++ Assets/Script/BattleScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class BattleScript : MonoBehaviour
 {
     public UnityEngine.UI.Image Enem;
+    public Animator EnemAnim;
     public Animator Punch;
     public Sprite Idle;
     public Sprite Hit1;
@@ -29,18 +30,21 @@ public class BattleScript : MonoBehaviour
         {
             Enem.sprite = Hit1;
             Punch.gameObject.SetActive(true);
+            EnemAnim.Play("피격1");
             Punch.Play("펀치1");
         }
         else if(rand == 1)
         {
             Enem.sprite = Hit2;
             Punch.gameObject.SetActive(true);
+            EnemAnim.Play("피격2");
             Punch.Play("펀치2");
         }
         else
         {
             Enem.sprite = Hit3;
             Punch.gameObject.SetActive(true);
+            EnemAnim.Play("피격3");
             Punch.Play("펀치3");
         }
     }
