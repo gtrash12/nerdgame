066204d8e983955265ae6813c39caaa3192a1ae3
jinhee-PC\jinhee <gtrash12@naver.com>
2019-08-14 21:29:06 +0900

    브금추가

--- Assets/Script/EventWindowControlScript.cs
+++ Assets/Script/EventWindowControlScript.cs
@@ -15,7 +15,7 @@ public class EventWindowControlScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        myRT.sizeDelta = new Vector2(myRT.sizeDelta.x, -1000-Relative.anchoredPosition.y);
+        myRT.sizeDelta = new Vector2(myRT.sizeDelta.x, -950-Relative.anchoredPosition.y);
         Debug.Log(myRT.sizeDelta);
     }
 }
